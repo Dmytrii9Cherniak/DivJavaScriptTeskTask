1d28dd9b5f25cf25622a30038c95f04f76f32cb9
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2023-07-13 10:08:28 +0300

    Added task files

---
+++ main.js
@@ -0,0 +1,35 @@
+const findRefEl = (container, index) => {
+    const targetIndex = +index;
+    for (let i = container.children.length - 1; i >= 0; i--) {
+        const currentIndex = +container.children[i].dataset.originalIndex;
+        if (currentIndex < targetIndex) {
+            return container.children[i];
+        }
+    }
+    return null;
+}
+
+const handleMove = (event) => {
+    const div = event.target, containerEl = div.closest('.container');
+    if (div.dataset.moved === 'false') {
+        containerEl.insertBefore(div, containerEl.firstElementChild);
+        div.dataset.moved = 'true';
+    } else {
+        const ref = findRefEl(containerEl, div.dataset.originalIndex);
+        containerEl.insertBefore(div, ref.nextElementSibling);
+        div.dataset.moved = 'false';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const containerEl = document.querySelector('.container');
+    for (let i = 0; i < 100; i++) {
+        const div = document.createElement('div');
+        div.classList.add('div-element');
+        div.innerText = `div${i + 1}`;
+        div.dataset.originalIndex = i.toString();
+        div.dataset.moved = false;
+        div.addEventListener('click', handleMove);
+        containerEl.appendChild(div);
+    }
+});
